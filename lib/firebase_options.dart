06326f6c40ed938064121a0c422a9d849c76f013
GUINO225 <stephanegnahoua892@gmail.com>
2025-08-27 23:38:59 +0000

    Update firebase_options.dart

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -2,9 +2,9 @@ import 'package:firebase_core/firebase_core.dart';
 
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform => const FirebaseOptions(
-        apiKey: 'YOUR_API_KEY',
-        appId: 'YOUR_APP_ID',
-        messagingSenderId: 'YOUR_SENDER_ID',
-        projectId: 'YOUR_PROJECT_ID',
+        apiKey: 'AIzaSyATko5Si9KUY3y3Auz-Ow-tpTMkktjEt8E',
+        appId: '1:78598100276:android:753a0038044fca59d3c161',
+        messagingSenderId: '78598100276',
+        projectId: 'civexam-bfc8d',
       );
 }
