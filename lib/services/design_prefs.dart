bc92f0479c7b01d0d50d3c184f8d6b2919eb1352
GUINO225 <stephanegnahoua892@gmail.com>
2025-08-28 12:24:06 +0000

    Test tile icon size and center prefs

--- lib/services/design_prefs.dart
+++ lib/services/design_prefs.dart
@@ -15,21 +15,26 @@ class DesignPrefs {
   static const _kBgPal   = 'design_bgPaletteName'; // NEW
   static const _kBgGrad  = 'design_bgGradient';
   static const _kDark    = 'design_darkMode';
+  static const _kTileSize = 'design_tileIconSize';
+  static const _kTileCtr  = 'design_tileCenter';
 
   static Future<DesignConfig> load() async {
     final p = await SharedPreferences.getInstance();
-    return DesignConfig(
-      useMono: p.getBool(_kUseMono) ?? const DesignConfig().useMono,
-      iconSetName: p.getString(_kIconSet) ?? const DesignConfig().iconSetName,
-      svgIconSize: p.getDouble(_kSvgSize) ?? const DesignConfig().svgIconSize,
-      monoColor: Color(p.getInt(_kMonoCol) ?? const DesignConfig().monoColor.value),
-      glassBlur: p.getDouble(_kBlur) ?? const DesignConfig().glassBlur,
-      glassBgOpacity: p.getDouble(_kBgOp) ?? const DesignConfig().glassBgOpacity,
-      glassBorderOpacity: p.getDouble(_kBdOp) ?? const DesignConfig().glassBorderOpacity,
-      waveEnabled: p.getBool(_kWave) ?? const DesignConfig().waveEnabled,
-      bgPaletteName: p.getString(_kBgPal) ?? const DesignConfig().bgPaletteName,
-      bgGradient: p.getBool(_kBgGrad) ?? const DesignConfig().bgGradient,
-      darkMode: p.getBool(_kDark) ?? const DesignConfig().darkMode,
+    const base = DesignConfig();
+    return base.copyWith(
+      useMono: p.getBool(_kUseMono) ?? base.useMono,
+      iconSetName: p.getString(_kIconSet) ?? base.iconSetName,
+      svgIconSize: p.getDouble(_kSvgSize) ?? base.svgIconSize,
+      monoColor: Color(p.getInt(_kMonoCol) ?? base.monoColor.value),
+      glassBlur: p.getDouble(_kBlur) ?? base.glassBlur,
+      glassBgOpacity: p.getDouble(_kBgOp) ?? base.glassBgOpacity,
+      glassBorderOpacity: p.getDouble(_kBdOp) ?? base.glassBorderOpacity,
+      waveEnabled: p.getBool(_kWave) ?? base.waveEnabled,
+      bgPaletteName: p.getString(_kBgPal) ?? base.bgPaletteName,
+      bgGradient: p.getBool(_kBgGrad) ?? base.bgGradient,
+      darkMode: p.getBool(_kDark) ?? base.darkMode,
+      tileIconSize: p.getDouble(_kTileSize) ?? base.tileIconSize,
+      tileCenter: p.getBool(_kTileCtr) ?? base.tileCenter,
     );
   }
 
@@ -46,5 +51,7 @@ class DesignPrefs {
     await p.setString(_kBgPal, cfg.bgPaletteName);
     await p.setBool(_kBgGrad, cfg.bgGradient);
     await p.setBool(_kDark, cfg.darkMode);
+    await p.setDouble(_kTileSize, cfg.tileIconSize);
+    await p.setBool(_kTileCtr, cfg.tileCenter);
   }
 }
