118aa620ec56499c17592223e4d8b6199dd5d671
GUINO225 <stephanegnahoua892@gmail.com>
2025-09-14 02:18:01 +0000

    Remove duplicate ENA questions and enforce uniqueness

--- test/question_bank_validation_test.dart
+++ test/question_bank_validation_test.dart
@@ -5,7 +5,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:civexam_app/data/ena_taxonomy.dart';
 
 void main() {
-  test('all questions have valid subject and chapter', () async {
+  test('all questions have valid subject/chapter and no duplicates', () async {
     final file = File('assets/questions/civexam_questions_ena_core.json');
     final raw = await file.readAsString();
     final data = json.decode(raw);
@@ -17,15 +17,28 @@ void main() {
     };
 
     final errors = <String>[];
+    final seenIds = <String>{};
+    final seenQuestions = <String>{};
 
     for (final item in data) {
       if (item is Map<String, dynamic>) {
+        final id = item['id']?.toString() ?? '';
         final subject = item['subject']?.toString() ?? '';
         final chapter = item['chapter']?.toString() ?? '';
+        final question = item['question']?.toString() ?? '';
+
+        if (!seenIds.add(id)) {
+          errors.add('Duplicate id "$id"');
+        }
+
+        if (!seenQuestions.add(question)) {
+          errors.add('Duplicate question "$question"');
+        }
+
         if (!subjectChapters.containsKey(subject)) {
-          errors.add('Unknown subject "$subject" (id: ${item['id']})');
+          errors.add('Unknown subject "$subject" (id: $id)');
         } else if (!subjectChapters[subject]!.contains(chapter)) {
-          errors.add('Unknown chapter "$chapter" for subject "$subject" (id: ${item['id']})');
+          errors.add('Unknown chapter "$chapter" for subject "$subject" (id: $id)');
         }
       } else {
         errors.add('Invalid question entry: $item');
